70d5fec504f2c9e27e00b7950f390b8a08f7153e
Jefferson Pavarin <jefferson.pavarin@gmail.com>
2024-10-02 21:22:02 -0300

    feat: project init

---
+++ middlewarepokeapi/src/main/java/com/pavarin/middlewarepokeapi/MiddlewarepokeapiApplication.java
@@ -0,0 +1,13 @@
+package com.pavarin.middlewarepokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewarepokeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewarepokeapiApplication.class, args);
+	}
+
+}
---
+++ middlewarepokeapi/src/test/java/com/pavarin/middlewarepokeapi/MiddlewarepokeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavarin.middlewarepokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewarepokeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
